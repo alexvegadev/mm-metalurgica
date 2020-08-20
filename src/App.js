108ec5babba8e428bd877c46f04c806e66fd68cb
Alex G. Peiretti Vega <alexvegadev@gmail.com>
2020-08-20 17:15:04 -0300

    Footer, and contact

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import Typography from '@material-ui/core/Typography'
 import './App.css';
 import Bar from './components/Bar/Bar'
 import Logo from './components/Logo/Logo'
@@ -20,6 +20,11 @@ function App() {
           <Route path="/works" component={Works} />
           <Route path="/contact" component={Contact} />
         </header>
+        <footer>
+          <div class="footer">
+              <h2>M&M Metalúrgica - San Luis Argentina - { new Date().getFullYear() }</h2>
+          </div>
+        </footer>
       </div>
     </Router>
   );
